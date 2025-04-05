f2e0c2cf0e1d03c186b16e34a57149fd6f3a25b4
Churiseki <churiseki921118@gmail.com>
2025-04-05 19:43:13 +0800

    File KomaLi Updated

---
+++ KomaLi/mgsm.py
@@ -0,0 +1,49 @@
+import openai
+import pandas as pd
+import json
+from tqdm import tqdm
+from api import api_key
+import nums_from_string as nfs
+
+openai.api_key = api_key
+dirs = ['mgsm_en', 'mgsm_zh']
+nums = 250
+prompt = "\n請在輸出的最後輸出答案，最後的輸出只能有數字"
+
+def handle_dir(dir):
+    df = pd.read_csv(f'./data/mgsm/{dir}.tsv', sep = '\t', nrows=nums, names=['question', 'answer'])
+    df.to_json(f'./data/mgsm/{dir}_{nums}.json', index=False, indent=2)
+    with open(f'./data/mgsm/{dir}_{nums}.json', 'r') as f:
+        data = json.load(f)
+    
+    result = []
+    for i in tqdm(range(nums)):
+        text = data['question'][str(i)] + prompt
+        response = openai.ChatCompletion.create(
+            model="gpt-4o-mini-2024-07-18",
+            messages=[{"role": "user", "content": text}],
+            temperature=0.2
+        )
+        result.append({"index": i, 
+                        "output": response["choices"][0]["message"]["content"],
+                        "answer": data['answer'][str(i)],
+                        "simplified": str(nfs.get_nums(response["choices"][0]["message"]["content"])[-1]),
+                        "question": text,
+                        "correct":True if nfs.get_nums(str(data['answer'][str(i)]))[-1] == nfs.get_nums(response["choices"][0]["message"]["content"])[-1] else False
+                    })
+    if dir == 'mgsm_en':
+        with open(f'./KomaLi/result/mgsm/{dir}_{nums}.json', 'w') as f:
+            json.dump(result, f, indent=2)
+    else:
+        with open(f'./KomaLi/result/mgsm/{dir}_{nums}.json', 'w', encoding='utf-8') as f:
+            json.dump(result, f, indent=2, ensure_ascii=False)
+
+
+
+def main():
+    for dir in dirs:
+        handle_dir(dir)
+
+
+if __name__ == '__main__':
+    main()
