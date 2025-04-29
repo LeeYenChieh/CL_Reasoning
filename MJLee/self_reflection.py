03924f6c115afe6906b8476453768cb6813f96ee
mjlee <yenchieh0826@gmail.com>
2025-04-29 20:34:00 +0800

    experiment 21

--- MJLee/self_reflection.py
+++ MJLee/self_reflection.py
@@ -6,25 +6,24 @@ import nums_from_string as nfs
 
 textWithoutProblem = f'Please translate it into English and Chinese. During the translation, continuously compare the translation with the original problem to ensure accuracy. Do not attempt to solve the problem during the translation process; only focus on translation.\n' \
 f'After completing the translations, treat the two problems as separate problems and solve them in their respective languages. For example, solve the Chinese problem in Chinese, the English problem in English, and so on.\n' \
-f'You are now going to simulate two AI assistants, each operating in a different language: a Chinese assistant and an English assistant must independently think and solve the problem based solely on the version of the question written in their respective language. Please simulate the behavior of the two assistants one by one, ensuring complete independence between them. \n' \
+f'You are now going to simulate four AI assistants, each operating in a different language: a Chinese assistant and an English assistant must independently think and solve the problem based solely on the version of the question written in their respective language. Please simulate the behavior of the two assistants one by one, ensuring complete independence between them. \n' \
 f'When solving a problem in one language, please ignore all your previous answers to this question. Think about it again from scratch and answer it as if you\'re seeing it for the first time. Do not refer to the answers in other languages. For example, do not refer to the English versions when solving the Chinese problem. Likewise, when solving the English problem, do not reference other versions.\n' \
-f'When solving a problem in a specific language, you must forget the solution steps and answers from other languages. "Forgetting" is defined as deactivating the tokens where that information is stored. After completing the solution steps and answer in one language, you need to compare them with those in other languages. If the processes in two languages are found to be too similar, you must regenerate the solution steps and answer for the current language.\n' \
 f'You must think during the problem-solving process; do not simply output the number without a solving process. At the end of each solution, output only the final answer, and the final answer must be in Arabic numeral format (0, 1, 2, 3, 4, 5, 6, 7, 8, 9).\n' \
 f'Once all two language versions are solved, compare the answers and processes to see if they are the same or different. If they differ, identify the incorrect process and answer. If they are the same, ensure the process and answer are correct.\n' \
 f'After comparison, confirm one final correct answer. At the very end of the output, only the final answer should be shown, and it must be in Arabic numeral format (0, 1, 2, 3, 4, 5, 6, 7, 8, 9).\n'
 
 outputFormat = f'You must strictly follow the output format below.\n' \
 f'Format:\n' \
-f'Chinese Problem\n' \
-f'{{Chinese Problem}}\n\n' \
 f'English Problem\n' \
 f'{{English Problem}}\n\n' \
-f'Chinese Answer\n' \
-f'{{Chinese Answer}}\n' \
-f'{{Chinese Final Answer}}\n\n' \
+f'Chinese Problem\n' \
+f'{{Chinese Problem}}\n\n' \
 f'English Answer\n' \
 f'{{English Answer}}\n' \
 f'{{English Final Answer}}\n\n' \
+f'Chinese Answer\n' \
+f'{{Chinese Answer}}\n' \
+f'{{Chinese Final Answer}}\n\n' \
 f'Compare Answer\n' \
 f'{{Compare Answer}}\n\n' \
 f'Final Answer\n' \
@@ -56,7 +55,7 @@ def self_reflection(data1, data2, result1, result2):
                         "answer": data2['answer'][str(i)],
                         "correct":correct,
         })
-    with open(f'./MJLee/result/mgsm/experiment22.json', 'w', encoding='utf-8') as f:
+    with open(f'./MJLee/result/mgsm/experiment21.json', 'w', encoding='utf-8') as f:
         json.dump(result, f, indent=2, ensure_ascii=False)
 
     print(f'total：{cnt}/{nums}')
