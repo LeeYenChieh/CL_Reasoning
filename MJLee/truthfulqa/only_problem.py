1a1e0e257345b58ba19da6fecc935f7d5b08037c
mjlee <yenchieh0826@gmail.com>
2025-05-26 18:04:18 +0800

    truthfulqa and mgsm experiment

--- MJLee/truthfulqa/only_problem.py
+++ MJLee/truthfulqa/only_problem.py
@@ -8,12 +8,14 @@ from datasets import load_dataset
 client = OpenAI(api_key=gpt_api_key)
 nums = 500
 model = "gpt-4o-mini-2024-07-18"
+letters = ['A', 'B', 'C', 'D']
 
 def self_reflection(dataset):
     result = [{
         "model": model
     }]
-    cnt = 0
+    correctCnt = 0
+    notSureCnt = 0
     for i in tqdm(range(nums)):
         problem = f'There is a Problem: \n{dataset[i]["question"]}.\n' \
         f'And there are four choices\n' \
@@ -31,9 +33,18 @@ def self_reflection(dataset):
         print(problem)
         print("=" * 40)
         print(response.choices[0].message.content)
-        correct = True if int(dataset[i]["answers_spans"]["spans"][0]) == nfs.get_nums(response.choices[0].message.content)[-1] else False
-        if correct:
-            cnt += 1
+        correct = False
+        try:
+            answer = letters[dataset[i]["mc1_targets"]["labels"].index(1)]
+            output = response.choices[0].message.content.split('\n')[-1]
+            if answer == output:
+                correct = True
+                correctCnt += 1
+        except:
+            correct = "true/false"
+            notSureCnt += 1
+        print(correct)
+        
         result.append({"index": i, 
                         "question": problem,
                         "output": response.choices[0].message.content,
@@ -43,12 +54,13 @@ def self_reflection(dataset):
     with open(f'./MJLee/truthfulqa/result/experiment1.json', 'w', encoding='utf-8') as f:
         json.dump(result, f, indent=2, ensure_ascii=False)
 
-    print(f'total：{cnt}/{nums}')
+    print(f'correct：{correctCnt}/{nums}')
+    print(f'notSure{notSureCnt}/{nums}')
 
 def main():
     dataset = load_dataset("truthfulqa/truthful_qa", "multiple_choice", split="validation")
     self_reflection(dataset)
-    
+
 
 if __name__ == '__main__':
     main()
