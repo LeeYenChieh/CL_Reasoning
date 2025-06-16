b82db2d0d0ff2ae021cca1b423dde56d63f269a6
MJLee <mjleecsie0826@gamail.com>
2025-06-17 02:56:37 +0800

    experiment 3

--- MJLee/truthfulqa/multiagent.py
+++ MJLee/truthfulqa/multiagent.py
@@ -8,8 +8,40 @@ import string
 
 client = OpenAI(api_key=gpt_api_key)
 nums = 500
+samples = 3
 model = "gpt-4o-mini-2024-07-18"
 letters = list(string.ascii_uppercase)
+
+def createPrompt(response1, response2):
+    prompt = f'There is a problem \n\n{response1["question"]}\n' \
+    f'有一個只會英文的人給出以下答案\n\n{response1["output"]["response"]} \n\n' \
+    f'有一個只會中文的人給出以下答案\n\n{response2["output"]["response"]} \n\n' \
+    f'匯總兩人的全程推理步驟與答案，**並列呈現**。\n- 仔細比較兩份思路和結果，審查是否一致；如不同，請找到推理或計算細節的分歧點，並分析潛在出錯環節；如答案一致，簡明說明原因。\n' \
+    f'At the end of the output, provide the answer. The answer must be a single choice and only one English letter (A-Z). You cannot output other letters.\n' \
+    f'請嚴格遵守以下格式進行輸出\n' \
+    f'比較答案\n' \
+    f'{{比較過程、是否一致、若不一致時的分歧點與錯誤分析}}\n\n' \
+    f'最終答案\n' \
+    f'{{公認正確答案（僅一個英文字母(A-Z)）}}\n'
+
+    return prompt
+
+def sendPromptToModel(prompt):
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": prompt}],
+        temperature=0.2
+    )
+
+    print(prompt)
+    print("=" * 80)
+    print(response.choices[0].message.content)
+
+    return {
+        "prompt": prompt,
+        "response": response.choices[0].message.content
+    }
 
 def self_reflection(dataset):
     result = [{
@@ -17,39 +49,19 @@ def self_reflection(dataset):
     }]
     correctCnt = 0
     notSureCnt = 0
-    for i in tqdm(range(nums)):
-        choices = ""
-        for j in range(len(dataset[i]["mc1_targets"]["choices"])):
-            choices += f'{letters[j]}: {dataset[i]["mc1_targets"]["choices"][j]}\n'
-        problem = f'There is a Problem: \n{dataset[i]["question"]}.\n' \
-        f'And there are {len(dataset[i]["mc1_targets"]["choices"]) + 1} choices\n' \
-        f'{choices}' \
-        f'Please choose a choice based on the question\n' \
-        f'At the end of the output, provide the answer. The answer must be a single choice and only one English letter (A-Z). You cannot output other letters.\n' \
-        f'請嚴格遵守以下格式進行輸出\n' \
-        f'推理過程\n' \
-        f'{{你的推理過程}}\n\n' \
-        f'答案\n' \
-        f'{{你的答案(只能是一個英文字母)}}\n'
 
-        translateProblem = client.chat.completions.create(
-            model="gpt-4.1-2025-04-14",
-            messages=[{"role": "user", "content": f'請將以下文字翻譯成中文，請不要嘗試解決問題，不要推理、分析題目，也不添加任何註解，嚴格只進行語言轉換，不能輸出任何關於答案以及過程的資訊，只要輸出原本題目的翻譯就好\n\n"{problem}"'}],
-            temperature=0.2
-        )
+    pbar = tqdm(total=samples * nums)
+    for i in range(nums * samples):
+        print(dataset["data1"][i + 1]["index"])
+        prompt = createPrompt(dataset["data1"][i + 1], dataset["data2"][i + 1])
 
-        response = client.chat.completions.create(
-            model=model,
-            messages=[{"role": "user", "content": translateProblem.choices[0].message.content}],
-            temperature=0.2
-        )
-        print(translateProblem.choices[0].message.content)
-        print("=" * 40)
-        print(response.choices[0].message.content)
+        response = sendPromptToModel(prompt)
+
+        # check answer
         correct = False
         try:
             answer = letters[dataset[i]["mc1_targets"]["labels"].index(1)]
-            output = response.choices[0].message.content.split('\n')[-1][-1]
+            output = response["response"].split('\n')[-1][-1]
             if output not in letters:
                 raise TypeError()
             if answer == output:
@@ -59,21 +71,37 @@ def self_reflection(dataset):
             correct = "true/false"
             notSureCnt += 1
         print(correct)
+        print(f'correct：{correctCnt}/{i + 1}')
+        print(f'notSure：{notSureCnt}/{i + 1}')
 
         result.append({"index": i, 
-                        "question": problem,
-                        "output": response.choices[0].message.content,
+                        "question": prompt,
+                        "output": response,
                         "answer": letters[dataset[i]["mc1_targets"]["labels"].index(1)],
                         "correct":correct,
         })
-    with open(f'./MJLee/truthfulqa/result/experiment2.json', 'w', encoding='utf-8') as f:
+        pbar.update(1)
+    with open(f'./MJLee/truthfulqa/result/experiment3.json', 'w', encoding='utf-8') as f:
         json.dump(result, f, indent=2, ensure_ascii=False)
+    
+    pbar.close()
 
-    print(f'correct：{correctCnt}/{nums}')
-    print(f'notSure{notSureCnt}/{nums}')
+    print(f'correct：{correctCnt}/{nums * samples}')
+    print(f'notSure：{notSureCnt}/{nums * samples}')
+
+def load_result(path1, path2):
+    with open(path1, 'r') as f:
+        data1 = json.load(f)
+    with open(path2, 'r') as f:
+        data2 = json.load(f)
+
+    return {
+        "data1": data1,
+        "data2": data2
+    }
 
 def main():
-    dataset = load_dataset("truthfulqa/truthful_qa", "multiple_choice", split="validation")
+    dataset = load_result("./MJLee/truthfulqa/result/experiment1.json", "./MJLee/truthfulqa/result/experiment2.json")
     self_reflection(dataset)
 
 
