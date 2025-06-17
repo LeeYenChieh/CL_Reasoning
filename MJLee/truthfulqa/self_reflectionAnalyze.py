659130452f11196ade133a92dc809536e4bb2a4f
mjlee <yenchieh0826@gmail.com>
2025-06-17 22:52:07 +0800

    some patch

---
+++ MJLee/truthfulqa/self_reflectionAnalyze.py
@@ -0,0 +1,52 @@
+import json
+import matplotlib.pyplot as plt
+
+def load_result(path1, path2, path3, path4):
+    with open(path1, 'r') as f:
+        data1 = json.load(f)
+    with open(path2, 'r') as f:
+        data2 = json.load(f)
+    with open(path3, 'r') as f:
+        data3 = json.load(f)
+    with open(path4, 'r') as f:
+        data4 = json.load(f)
+
+    return {
+        "data1": data1,
+        "data2": data2,
+        "data3": data3,
+        "data4": data4,
+    }
+
+def main():
+    dataset = load_result("./MJLee/truthfulqa/result/experiment1.json", "./MJLee/truthfulqa/result/experiment2.json", "./MJLee/truthfulqa/result/experiment3.json", "./MJLee/truthfulqa/result/experiment4.json")
+    ttt, ttf, fft, fff, tft, tff, ftt, ftf = 0, 0, 0, 0, 0, 0, 0, 0
+    for i in range(1, 1501):
+        if dataset["data1"][i]["correct"] and dataset["data2"][i]["correct"] and dataset["data4"][i]["correct"]:
+            ttt += 1
+        elif dataset["data1"][i]["correct"] and dataset["data2"][i]["correct"] and not dataset["data4"][i]["correct"]:
+            ttf += 1
+        elif not dataset["data1"][i]["correct"] and not dataset["data2"][i]["correct"] and dataset["data4"][i]["correct"]:
+            fft += 1
+        elif not dataset["data1"][i]["correct"] and not dataset["data2"][i]["correct"] and not dataset["data4"][i]["correct"]:
+            fff += 1
+        elif dataset["data1"][i]["correct"] and not dataset["data2"][i]["correct"] and dataset["data4"][i]["correct"]:
+            tft += 1
+        elif dataset["data1"][i]["correct"] and not dataset["data2"][i]["correct"] and not dataset["data4"][i]["correct"]:
+            tff += 1
+        elif not dataset["data1"][i]["correct"] and dataset["data2"][i]["correct"] and dataset["data4"][i]["correct"]:
+            ftt += 1
+        elif not dataset["data1"][i]["correct"] and dataset["data2"][i]["correct"] and not dataset["data4"][i]["correct"]:
+            ftf += 1
+
+    labels = ["True", "False"]
+    sizestt = [ttt / (ttf + ttt), ttf / (ttf + ttt)]
+    sizesff = [fft / (fff + fft), fff / (fff + fft)]
+    sizestf = [tft / (tff + tft), tff / (tff + tft)]
+    sizesft = [ftt / (ftf + ftt), ftf / (ftf + ftt)]
+
+    plt.pie(sizesff, labels=labels, autopct='%1.1f%%', startangle=100)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
---
+++ MJLee/truthfulqa/self_reflectionAnalyzeSelf.py
@@ -0,0 +1,75 @@
+import json
+import matplotlib.pyplot as plt
+import string
+
+def load_result(path):
+    with open(path, 'r') as f:
+        data = json.load(f)
+
+    return data
+
+def checkCorrect(response, answer):
+    letters = list(string.ascii_uppercase)
+    correct = False
+    try:
+        output = response.split()[-1][-1]
+        if output not in letters:
+            raise TypeError()
+        if answer == output:
+            correct = True
+    except:
+        correct = "true/false"
+    return correct, output
+
+def main():
+    dataset = load_result("./MJLee/truthfulqa/result/experiment4.json")
+
+    ttt, ttf, fft, fff, tft, tff, ftt, ftf = 0, 0, 0, 0, 0, 0, 0, 0
+    same = 0
+    for i in range(1, 1501):
+        responses = dataset[i]["output"]["response"].split("—————————————")
+        if len(responses) != 4:
+            print(i)
+        
+        CN_correct, CN_answer = checkCorrect(responses[1], dataset[i]["answer"])
+        EN_correct, EN_answer = checkCorrect(responses[2], dataset[i]["answer"])
+
+        if EN_correct and CN_correct and dataset[i]["correct"]:
+            ttt += 1
+        elif EN_correct and CN_correct and not dataset[i]["correct"]:
+            ttf += 1
+        elif not EN_correct and not CN_correct and dataset[i]["correct"]:
+            fft += 1
+        elif not EN_correct and not CN_correct and not dataset[i]["correct"]:
+            fff += 1
+        elif EN_correct and not CN_correct and dataset[i]["correct"]:
+            tft += 1
+        elif EN_correct and not CN_correct and not dataset[i]["correct"]:
+            tff += 1
+        elif not EN_correct and CN_correct and dataset[i]["correct"]:
+            ftt += 1
+        elif not EN_correct and CN_correct and not dataset[i]["correct"]:
+            ftf += 1
+        
+        if EN_answer != CN_answer:
+            same += 1
+            print(i)
+
+    print(ttf + ttt)
+    print(fff + fft)
+    print(tff + tft)
+    print(ftf + ftt)
+    print(same)
+
+    labels = ["True", "False"]
+    sizestt = [ttt / (ttf + ttt), ttf / (ttf + ttt)]
+    sizesff = [fft / (fff + fft), fff / (fff + fft)]
+    sizestf = [tft / (tff + tft), tff / (tff + tft)]
+    sizesft = [ftt / (ftf + ftt), ftf / (ftf + ftt)]
+
+    
+    # plt.pie(sizesff, labels=labels, autopct='%1.1f%%', startangle=100)
+    # plt.show()
+
+if __name__ == '__main__':
+    main()
