f28c6fd193750c0cb55dfe5b55679a54e4535e8c
MJLee <mjleecsie0826@gamail.com>
2025-07-15 15:33:53 +0800

    mathqa 1

---
+++ MJLee/mathqa/only_problem.py
@@ -0,0 +1,99 @@
+from openai import OpenAI
+import json
+from api import gpt_api_key
+from tqdm import tqdm
+import nums_from_string as nfs
+from datasets import load_dataset
+import string
+
+client = OpenAI(api_key=gpt_api_key)
+nums = 500
+samples = 1
+model = "gpt-4.1-mini-2025-04-14"
+letters = list(string.ascii_lowercase)
+
+def createPrompt(question, choices):
+    prompt = f'There is a Problem: \n{question}.\n' \
+    f'And there are 5 choices\n' \
+    f'{choices}\n' \
+    f'Please choose a choice based on the question\n' \
+    f'At the end of the output, provide the answer. The answer must be a single choice and only one English letter (a-e). You cannot output other letters.\n' \
+    f'請嚴格遵守以下格式進行輸出，並用英文回答\n' \
+    f'推理過程\n' \
+    f'{{你的推理過程}}\n\n' \
+    f'答案\n' \
+    f'{{你的答案(只能是一個英文字母)}}\n'
+
+    return prompt
+
+def sendPromptToModel(prompt):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": prompt}],
+        temperature=0
+    )
+
+    print(prompt)
+    print("=" * 80)
+    print(response.choices[0].message.content)
+
+    return {
+        "prompt": prompt,
+        "response": response.choices[0].message.content
+    }
+
+
+def self_reflection(dataset):
+    result = [{
+        "model": model
+    }]
+    correctCnt = 0
+    notSureCnt = 0
+
+    pbar = tqdm(total=samples * nums)
+    for i in range(nums):
+        prompt = createPrompt(dataset[i]["Problem"], dataset[i]["options"])
+
+        for j in range(samples):
+            response = sendPromptToModel(prompt)
+
+            # check answer
+            correct = False
+            try:
+                answer = dataset[i]["correct"]
+                output = response["response"].split('\n')[-1][-1]
+                if output not in letters:
+                    raise TypeError()
+                if answer == output:
+                    correct = True
+                    correctCnt += 1
+            except:
+                correct = "true/false"
+                notSureCnt += 1
+            print(correct)
+            print(f'correct：{correctCnt}/{i * samples + j + 1}')
+            print(f'notSure：{notSureCnt}/{i * samples + j + 1}')
+
+            result.append({"index": i * samples + j, 
+                            "question": prompt,
+                            "output": response,
+                            "answer": dataset[i]["correct"],
+                            "correct":correct,
+            })
+            pbar.update(1)
+    with open(f'./MJLee/truthfulqa/result/experiment13.json', 'w', encoding='utf-8') as f:
+        json.dump(result, f, indent=2, ensure_ascii=False)
+
+
+    pbar.close()
+
+    print(f'correct：{correctCnt}/{nums * samples}')
+    print(f'notSure：{notSureCnt}/{nums * samples}')
+
+def main():
+    dataset = load_dataset("allenai/math_qa", split="test")
+    self_reflection(dataset[0:nums])
+
+
+if __name__ == '__main__':
+    main()
