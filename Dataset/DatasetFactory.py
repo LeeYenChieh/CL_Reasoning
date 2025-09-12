4a5c41a4dd9dd640294ecf9304b1c2bb350f75d2
mjlee <yenchieh0826@gmail.com>
2025-09-12 19:07:20 +0800

    mv file

--- Dataset/DatasetFactory.py
+++ Dataset/DatasetFactory.py
@@ -6,6 +6,7 @@ class DatasetFactory():
         pass
 
     def buildDataset(self, type, *args, **kwargs) -> Dataset:
+        print(*args, **kwargs)
         if type == 'mathqa':
             return MathQA(*args, **kwargs)
         else:
