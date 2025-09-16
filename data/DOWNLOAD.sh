791ce73c087fcc7e396938ddd1a312bc7ef124c4
mjlee <yenchieh0826@gmail.com>
2025-09-16 18:14:39 +0800

    fix data download

--- data/DOWNLOAD.sh
+++ data/DOWNLOAD.sh
@@ -17,24 +17,16 @@ if [ ! -d "mmlu" ]; then
     echo "MMLU dataset extracted to mmlu/ directory"
 fi
 
-# Download TruthfulQA dataset  
-if [ ! -d "truthfulqa" ]; then
-    git clone https://github.com/sylinrl/TruthfulQA.git truthfulqa
-fi
-
 # Download XCOPA dataset
 if [ ! -d "xcopa" ]; then
     git clone https://github.com/cambridgeltl/xcopa.git xcopa
-fi
-
-# Download MGSM dataset (if zip file exists)
-if [ -f "mgsm.zip" ]; then
-    unzip mgsm.zip
 fi
 
 echo "Dataset download completed!"
 echo "Available datasets:"
 echo "- MathQA: mathqa.json"
 echo "- MMLU: mmlu/test/ (extracted from mmlu_data.tar)"
-echo "- TruthfulQA: truthfulqa/"
+echo "- TruthfulQA: truthfulqa/truthful_qa"
 echo "- XCOPA: xcopa/"
+echo "- MGSM: mgsm_en.json"
+echo "-CommenseQA: commenseqa.json"
