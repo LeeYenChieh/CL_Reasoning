c5cd76a21356fec602345bed2b241f1be7ae8e1e
MJLee <mjleecsie0826@gamail.com>
2025-09-17 02:42:52 +0800

    fix prompt

--- Strategy/multiAgent.py
+++ Strategy/multiAgent.py
@@ -47,7 +47,7 @@ class MultiAgent(Strategy):
         prompt = f'Please strictly follow the format below for output\n' \
             f'Reasoning process\n' \
             f'{{your reasoning process}}\n\n' \
-            f'Final Answer' \
+            f'Final Answer\n' \
             f'{{Your final Answer, Your response must end with the exact JSON format specified in the question above.}}\n'
         return prompt
     
--- Strategy/onlyChinese.py
+++ Strategy/onlyChinese.py
@@ -24,6 +24,7 @@ class OnlyChinese(Strategy):
         prompt = f'請嚴格遵守以下格式進行輸出\n' \
             f'推理過程\n' \
             f'{{你的推理過程}}\n\n' \
+            f'最終答案\n' \
             f'{{你的答案，必須完全符合上述問題指定的 JSON 格式，不能添加多餘文字或解釋}}\n'
         return prompt
 
