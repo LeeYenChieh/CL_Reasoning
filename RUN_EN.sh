0a4d268ec6969fb8ce9faff83b7d2c374c7d4d70
mjlee <yenchieh0826@gmail.com>
2025-11-17 14:41:54 +0800

    run sh

--- RUN_EN.sh
+++ RUN_EN.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-python3 ./main.py --run -m ${1} -d mathqa  -s onlyEnglish --nums 3000
-python3 ./main.py --run -m ${1} -d commenseqa -s onlyEnglish --nums 6000
-python3 ./main.py --run -m ${1} -d mmlu -s onlyEnglish --nums 6000
-python3 ./main.py --run -m ${1} -d truthfulqa -s onlyEnglish --nums 6000
+python3 ./main.py --run -m gpt4.1mini -d mathqa  -s onlyEnglish --nums 3000
+python3 ./main.py --run -m gpt4.1mini -d commenseqa -s onlyEnglish --nums 6000
+python3 ./main.py --run -m gpt4.1mini -d mmlu -s onlyEnglish --nums 6000
+python3 ./main.py --run -m gpt4.1mini -d truthfulqa -s onlyEnglish --nums 6000
+
+python3 ./main.py --run -m deepseek -d mathqa  -s onlyEnglish --nums 3000
+python3 ./main.py --run -m deepseek -d commenseqa -s onlyEnglish --nums 6000
+python3 ./main.py --run -m deepseek -d mmlu -s onlyEnglish --nums 6000
+python3 ./main.py --run -m deepseek -d truthfulqa -s onlyEnglish --nums 6000
+
+python3 ./main.py --run -m qwen mathqa  -s onlyEnglish --nums 3000
+python3 ./main.py --run -m qwen commenseqa -s onlyEnglish --nums 6000
+python3 ./main.py --run -m qwen mmlu -s onlyEnglish --nums 6000
+python3 ./main.py --run -m qwen truthfulqa -s onlyEnglish --nums 6000
